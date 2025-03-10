75fee6ee4af6e2669d69664a817653bea8383a97
lakshmipathibellam <lakshmipathi@puropalecreations.com>
2025-03-10 12:53:19 +0530

    new change

--- feb.py
+++ feb.py
@@ -2,7 +2,7 @@ n = int(input("Enter the number of terms: "))
 a, b = 0, 1
 print("Fibonacci Sequence:")
 print(a, b, end=" ")
-for _ in range(n - 2):  
+for i in range(n - 2):  
     c = a + b
     print(c, end=" ")
     a, b = b, c
